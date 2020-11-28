d64ab6e7c8027114d302ad7d00565e96fa04da07
tanguiRobin <nakim@gmx.fr>
2020-11-28 10:00:11 +0100

    integration dockerfil & sonar

--- Code/SpringSocial/src/main/java/com/spring/social/ApplicationSocial.java
+++ Code/SpringSocial/src/main/java/com/spring/social/ApplicationSocial.java
@@ -1,7 +1,13 @@
 package com.spring.social;
 
+import java.util.Locale;
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.servlet.LocaleResolver;
+import org.springframework.web.servlet.i18n.CookieLocaleResolver;
+import org.springframework.web.servlet.i18n.LocaleChangeInterceptor;
 
 @SpringBootApplication
 public class ApplicationSocial {
