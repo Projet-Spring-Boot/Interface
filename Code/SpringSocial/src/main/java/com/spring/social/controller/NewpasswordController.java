bd5a8c579463f1f9c04363302a63efe7f637a427
vgiauffret <vinc.giauffret@hotmail.fr>
2020-11-28 11:19:59 +0100

    Modification controller NewPassword

--- Code/SpringSocial/src/main/java/com/spring/social/controller/NewpasswordController.java
+++ Code/SpringSocial/src/main/java/com/spring/social/controller/NewpasswordController.java
@@ -1,16 +1,10 @@
 package com.spring.social.controller;
 
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
 
 @Controller
+@RequestMapping("/newpassword")
 public class NewpasswordController {
-	@RequestMapping(value = { "/newpassword" }, method = RequestMethod.GET)
-	public String login(Model model) {
-		
-		//model.addAttribute("host", "http://localhost:8081/login");
-		return "newpassword";
-	}
+
 }
