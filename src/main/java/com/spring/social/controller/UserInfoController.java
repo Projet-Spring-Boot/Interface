88e293c88369a60c2b6e03bbcadc600e27da3608
vgiauffret <vinc.giauffret@hotmail.fr>
2020-12-04 11:39:58 +0100

    Ajout affichage temps de connexion, et nombre de connexion

--- src/main/java/com/spring/social/controller/UserInfoController.java
+++ src/main/java/com/spring/social/controller/UserInfoController.java
@@ -1,6 +1,8 @@
 package com.spring.social.controller;
 
 import java.security.Principal;
+import java.util.Collections;
+import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
@@ -10,14 +12,19 @@ import org.springframework.web.bind.annotation.RequestMethod;
 
 import com.spring.social.dao.AppUserDAO;
 import com.spring.social.entity.AppUser;
+import com.spring.social.security.SecurityAuto;
+//import com.spring.social.dao.InfoConnectionDAO;
 
 @Controller
 @RequestMapping("/userInfo")
 public class UserInfoController {
-
+	
+	//private long connectionId = 0;
 	@Autowired
 	private AppUserDAO appUserDAO;
-
+	//@Autowired
+	//private InfoConnectionDAO infoConnectionDAO;
+	
 	@RequestMapping(method = RequestMethod.GET)
 	public String userInfo(Model model, Principal principal) {
 
@@ -25,7 +32,17 @@ public class UserInfoController {
 		AppUser logineduser2 = this.appUserDAO.findAppUserByUserName(principal.getName());
 
 		model.addAttribute("appUser", logineduser2);
+		//List<Long> list = infoConnectionDAO.getConnectionIdByUserId(logineduser2.getUserId());
+		//Long connectionId = Collections.max(list,null);
 		
+		//Long userId = logineduser2.getUserId()
+		//Long dureeSession = infoConnectionDAO.getElapsedTime(connectionId);
+		//Long nbConnexion = infoConnectionDAO.getNbConnection(userId)
+		Long dureeSession = (long) 5;
+		Long nbConnexion = (long) 2;
+		model.addAttribute("dureeSession", dureeSession);
+		model.addAttribute("nbConnexion", nbConnexion);
+
 		return "userInfoPage";
 	}
 }
